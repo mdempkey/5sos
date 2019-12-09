3a29b5af48314508b6bdabbfb6a9af552ec4a381
Martha D <45017656+mdempkey@users.noreply.github.com>
2019-12-09 08:31:53 -0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,30 @@
+var a = document.getElementById("luke");
+var b = document.getElementById("michael");
+var c = document.getElementById("calum");
+var d = document.getElementById("ash");
+var e = document.getElementById("young");
+
+function newImg(){
+	a.src="images/ezgif-6-38aabc66c892.gif";
+	a.classList.remove("hvr-glow");
+}
+
+function newImg1(){
+	b.src="images/ezgif-6-9cbefa22dfde.gif";
+	b.classList.remove("hvr-glow");
+}
+
+function newImg2(){
+	c.src="images/ezgif-6-e66a354a424f.gif";
+	c.classList.remove("hvr-glow");
+}
+
+function newImg3(){
+	d.src="images/ezgif-6-30056ffb951f.gif";
+	d.classList.remove("hvr-glow");
+}
+
+function newSound(){
+	play()
+}
+
